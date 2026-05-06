d23945fd83dac2546f9ada70dffed7a806161314
ane-noelle <anenoelle60@gmail.com>
2026-05-06 10:54:11 -0300

    dia2

--- EtecBus/App.js
+++ EtecBus/App.js
@@ -8,9 +8,39 @@ import * as Location from "expo-location";
 const SCHOOL = {
   id: 'school',
   name: 'Etec Comendador Joao Rays',
-  cordinate: {},
+  cordinate: { latitude: -22.48920909957289, longitude: -48.54640704593705},
   address: 'Rua Ludovico Victorio, 2140, Barra Bonita - SP'
 };
+
+const BUS_STOPS = [
+  {
+    id: 'stop_1',
+    name: 'Autoescola Muriano',
+    address: 'R. Geraldo Fazzio, 484',
+    cordinate: { latitude: -22.484226345567947, longitude: -48.564770398285106},
+    lines: ['Nova Barra'],
+  }
+];
+
+//Distancia Haversine (metros)
+function getDistance(c1,c2) {
+
+}
+
+function formaDistance(m) {
+  return m < 1000 ? `${Math.round(m)} m` : `${(m / 1000).toFixed(1)} km`; 
+}
+
+// HTML do leaflet (openstreetmap - sem chave)
+function buidLeafletHTML(userCoord, nearestStopId, selectedStopId) {
+  const stopsJSON = JSON.stringify(BUS_STOPS);
+  const schoolJSON = JSON.stringify(SCHOOL);
+  const userJSON =  userCoord ? JSON.stringify(userCoord) : 'null';
+
+  return ``;
+
+}
+
 
 export default function App() {
   return (
